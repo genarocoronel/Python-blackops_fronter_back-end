34d09774baabcb4e8f655801706006689329ecca
Genaro Coronel <genaro.coronel561@gmail.com>
2020-02-10 20:07:20 +0000

    BankEdms DataX & Docusign integration changes

--- app/main/service/bank_account_service.py
+++ app/main/service/bank_account_service.py
@@ -3,31 +3,101 @@ import datetime
 from app.main import db
 from app.main.model.bank_account import BankAccount
 from app.main.service.third_party.datax_service import validate_bank_account
+from app.main.model.bank_account import BankAccountValidationStatus, BankAccountValidationHistory
 
 
 def create_bank_account(client, data, override=False, overridable_codes=None):
-    result, error = validate_bank_account(data.get('account_number'), data.get('routing_number'))
+    account_number = data.get('account_number')
+    routing_number = data.get('routing_number')
+
+    result, error = validate_bank_account(account_number, routing_number)
     if error:
         return None, error
     else:
-        if result.get('valid') or (override and result.get('validation_code') in overridable_codes):
-            new_bank_account = BankAccount(
-                name=result.get('bank_name'),
-                account_number=result.get('account_number'),
-                routing_number=result.get('aba_number'),  # TODO: find out when/if I use 'NewRoutingNumber' or 'BankABA'
-                valid=result.get('valid'),
-                inserted_on=datetime.datetime.utcnow(),
-                client=client
-            )
+        code = result.get('code')
+        message = "Account validation passed" if result.get('valid') else "Account validation failed"
+        if code is not None:
+            bav_status = BankAccountValidationStatus.query.filter_by(code=code).first()
+            if bav_status is not None:
+                message = bav_status.description
 
-            save_changes(new_bank_account)
-            return new_bank_account, None
+        # create validation History
+        bav_history = BankAccountValidationHistory(client_id=client.id,
+                                                  account_number=account_number,
+                                                  routing_number=routing_number,
+                                                  timestamp=datetime.datetime.utcnow())               
+        if bav_status is not None:
+            bav_history.bav_status_id = bav_status.id
+        save_changes(bav_history) 
+
+        if result.get('valid') or (override and result.get('code') in overridable_codes):
+            acct_owner = data.get('owner_name')
+            acct_address = data.get('address')
+            acct_city = data.get('city')
+            acct_state = data.get('state')
+            acct_zip = data.get('zip')
+            acct_ssn = data.get('ssn')
+            acct_email = data.get('email')
+
+            bank_name = result.get('bank_name')
+            if bank_name is None:
+                bank_name = data.get('bank_name')
+            # check bank account exists	
+            bank_account = client.bank_account
+            if bank_account is None:
+                new_bank_account = BankAccount(
+                    bank_name=bank_name,
+                    account_number=result.get('account_number'),
+                    routing_number=result.get('aba_number'),  # TODO: find out when/if I use 'NewRoutingNumber' or 'BankABA'
+                    valid=result.get('valid'),
+                    inserted_on=datetime.datetime.utcnow(),
+                    client_id=client.id,
+                    owner_name = acct_owner,
+                    email = acct_email,
+                    address = acct_address, 
+                    city = acct_city,
+                    state = acct_state,
+                    zip = acct_zip,
+                    ssn = acct_ssn, 
+                )
+                if bav_status is not None:
+                    new_bank_account.bav_status_id = bav_status.id
+                
+                save_changes(new_bank_account)
+            else:
+                bank_account.bank_name = bank_name
+                bank_account.account_number = result.get('account_number')
+                bank_account.routing_number = result.get('aba_number')
+                bank_account.valid = True
+                bank_account.owner_name = acct_owner
+                bank_account.email = acct_email
+
+                db.session.commit()
+            
+            data['bank_name'] = bank_name
+            data['account_number'] = result.get('account_number')
+            data['routing_number'] = result.get('aba_number')
+            result = {
+                'aba_valid': True,
+                'acct_valid': True,
+                'valid': True,
+                'code': code,
+                'message': message,
+                'bank_account': data,
+            }
+            return result, None
         else:
-            validation_code = result.get('validation_code')
-            if override:
-                return None, dict(message='Error requires manager/admin for override', error_code=validation_code)
-            return None, dict(message=result.get('validation_detail'), error_code=validation_code)
 
+            result = {
+                'aba_valid': result.get('aba_valid'),
+                'acct_valid': result.get('acct_valid'),
+                'valid': False,
+                'code': code,
+                'message': message,
+                'bank_account': {},
+            }
+            print(result)
+            return result, None
 
 def save_changes(*data):
     for entry in data:
