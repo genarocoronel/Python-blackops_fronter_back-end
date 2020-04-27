20372922c8a83ec231529f51091fe811a5d28e5e
Genaro Coronel <genaro.coronel561@gmail.com>
2020-04-27 18:31:51 +0000

    Redis charset issue & token header changed to Authorization

--- app/main/channels/channel.py
+++ app/main/channels/channel.py
@@ -23,27 +23,27 @@ class Channel(Namespace):
                 # storage id
                 u_key = "channels:user:{}".format(user.id)
                 # update the storage
-                app.redis.set(u_key, request.sid, ex=self.CHANNELS_STORAGE_EXPIRY)
+                app.chnls_key_store.set(u_key, request.sid, ex=self.CHANNELS_STORAGE_EXPIRY)
                 # add the socket id into store
                 self.on_open(user)
            
     def on_disconnect(self):
-        if request.headers and 'token' in request.headers:
+        if request.headers and 'Authorization' in request.headers:
             # find the user using the token
-            token = request.headers['token']
+            token = request.headers['Authorization']
             user = Auth.get_user_from_token(token)
             if user:
                 # storage id
                 u_key = "channels:user:{}".format(user.id)
                 self.on_close(user)
                 # update the storage
-                app.redis.delete(u_key)
+                app.chnls_key_store.delete(u_key)
 
 
     @staticmethod
     def send_event(user_id, event_type, event_params, namespace):
         u_key = "channels:user:{}".format(user_id)
-        sid = app.redis.get(u_key) 
+        sid = app.chnls_key_store.get(u_key) 
         if sid:
             emit(event_type, event_params, room=sid, namespace=namespace)
 
