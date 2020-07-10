c563f2bdc3336fcd9ff34d5fa9cd7a0b2a2d33c3
Genaro Coronel <genaro.coronel561@gmail.com>
2020-07-10 20:59:20 +0000

    Campaign report generation

--- app/main/service/campaign_service.py
+++ app/main/service/campaign_service.py
@@ -2,7 +2,9 @@ from datetime import datetime
 import uuid
 
 from app.main.model.campaign import Campaign, MarketingModel, MailType, PinnaclePhoneNumber
-from .apiservice import ApiService
+from app.main.model.client import Client, ClientType, ClientCampaign
+from sqlalchemy import and_
+from .apiservice import ApiService, has_permissions
 
 class CampaignService(ApiService):
     _model = Campaign
@@ -68,3 +70,92 @@ class PinnaclePhoneNumService(ApiService):
     
     def _queryset(self):
         return self._model.query.all()
+
+from openpyxl import Workbook
+from openpyxl.styles import Font
+from openpyxl.styles import PatternFill
+from app.main.core.io import stream_file
+from app.main.config import upload_location as report_file_path
+
+class CampaignReportService(ApiService):
+    _heading = 'DIRECT MAIL ZIP PERFORMANCE SUMMARY'
+    _dealer  = 'ELITE DMS'
+    _report_name = 'campaign_report.xlsx'
+    
+    @has_permissions
+    def generate_report(self):
+        wb = Workbook()
+        ws = wb.active
+
+        # fill the data
+        ws['A1'] = self._heading
+        ws['A3'] = 'Dealer Name:'
+        ws['C3'] = self._dealer
+        ws['A4'] = 'Location:'
+        ws['A5'] = 'Report Date:'
+        ws['C5'] = datetime.utcnow().strftime('%m/%d/%Y')
+        # styling
+        ws['A1'].font = Font(bold=True)
+
+        ws.append([])
+        ws.append(['NAME OF CAMPAIGN', 'MAIL PIECES SENT', 'LEADS GENERATED', 'LEAD %', 'DEALS', 'DEAL %', 'TOTAL COST', 'COST/LEAD', 'COST/DEAL'])
+        col_head_style = PatternFill(start_color='0591f5', end_color='0591f5', fill_type = "solid")
+        for rows in ws.iter_rows(min_row=7, max_row=7, min_col=1):
+            for cell in rows:
+                cell.fill = col_head_style
+         
+        report = []
+        row_index = 7
+        for campaign in Campaign.query.all():
+            row = []
+            row_index = row_index + 1
+            if not campaign.num_mail_pieces:
+                campaign.num_mail_pieces = 0
+            if not campaign.cost_per_piece:
+                campaign.cost_per_piece = 0
+
+            # leads generated
+            leads_percent = 0 
+            num_leads = ClientCampaign.query.outerjoin(Client)\
+                                            .outerjoin(Campaign)\
+                                            .filter(and_(Campaign.id==campaign.id, Client.type==ClientType.lead)).count()
+
+            if num_leads > 0:
+                leads_percent = round((num_leads / campaign.num_mail_pieces) * 100, 2)
+            # deals
+            deals_percent = 0
+            num_clients = ClientCampaign.query.outerjoin(Client)\
+                                              .outerjoin(Campaign)\
+                                              .filter(and_(Campaign.id==campaign.id, Client.type==ClientType.client)).count()
+            if num_clients > 0:
+                deals_percent = round((num_clients / campaign.num_mail_pieces) * 100, 2) 
+            total_cost = campaign.num_mail_pieces * campaign.cost_per_piece 
+
+            cost_per_lead = round(total_cost / num_leads, 2) if num_leads > 0 else 0
+            cost_per_deal = round(total_cost / num_clients, 2) if num_clients > 0 else 0
+
+            row.append(campaign.name)
+            row.append(campaign.num_mail_pieces)
+            row.append(num_leads)
+            row.append(leads_percent)
+            row.append(num_clients)
+            row.append(deals_percent)
+            row.append(total_cost)
+            row.append(cost_per_lead)
+            row.append(cost_per_deal)
+            ws.append(row) 
+        
+        t = row_index + 1 
+        ws["B{}".format(t)] = '=SUM(B8:B{})'.format(row_index)
+        ws["C{}".format(t)] = '=SUM(C8:C{})'.format(row_index)
+        ws["D{}".format(t)] = '=(C{}/B{})*100'.format(t, t)
+        ws["E{}".format(t)] = '=SUM(E8:E{})'.format(row_index)
+        ws["G{}".format(t)] = '=SUM(G8:G{})'.format(row_index)
+        # save the file    
+        report_file = "{}/{}".format(report_file_path, self._report_name)  
+        wb.save(filename=report_file)
+
+        return stream_file(report_file_path, self._report_name, as_attachment=False)
+
+        
+
