bd46caa6398c1a02624343f9b219fa92cfcb8463
Keith Homco <kphomco@gmail.com>
2020-08-19 10:05:39 -0400

    ability to view, update, and delete pbx systems

--- app/main/service/config_service.py
+++ app/main/service/config_service.py
@@ -8,12 +8,13 @@ from sqlalchemy import or_
 from werkzeug.exceptions import NotFound
 
 from app.main import db
+from app.main.core.helper import convert_to_boolean, is_boolean
 from app.main.model.contact_number import ContactNumberType
 from app.main.model.income import IncomeType
 from app.main.model.candidate import CandidateDisposition
 from app.main.model.client import ClientDisposition
 from app.main.model.monthly_expense import ExpenseType
-from app.main.model.pbx import PBXNumber
+from app.main.model.pbx import PBXNumber, PBXSystem
 from app.main.model.user import Department
 from app.main.service.docproc_service import get_docproc_types
 
@@ -40,8 +41,57 @@ def get_all_clients_dispositions():
     return ClientDisposition.query.filter_by().all()
 
 
+def get_pbx_systems(enabled=True):
+    pbx_systems_stmt = PBXSystem.query.filter()
+    if is_boolean(enabled):
+        is_enabled = convert_to_boolean(enabled)
+        pbx_systems_stmt = pbx_systems_stmt.filter_by(enabled=is_enabled)
+
+    return pbx_systems_stmt.all()
+
+
+def update_pbx_system(system_publuc_id:str, data):
+    pbx_system = PBXSystem.query.filter_by(public_id=system_publuc_id).first()
+    if not pbx_system :
+        raise NotFound
+
+    for attr in data:
+        if hasattr(pbx_system , attr):
+            setattr(pbx_system , attr, data.get(attr))
+
+    db.session.add(pbx_system )
+    db.session.commit()
+
+    return pbx_system
+
+
+def map_number_to_pbx_system(system_public_id:str, number_public_id:str):
+    pbx_system = PBXSystem.query.filter_by(public_id=system_public_id).first()
+    pbx_number = PBXNumber.query.filter_by(public_id=number_public_id).first()
+
+    if pbx_system is None or pbx_number is None:
+        raise NotFound
+
+    pbx_number.pbx_system = pbx_system
+    db.session.add(pbx_number)
+    db.session.commit()
+
+
+def delete_pbx_system(public_id:str):
+    pbx_system = PBXSystem.query.filter_by(public_id=public_id).first()
+    if pbx_system is None:
+        raise NotFound
+
+    db.session.delete(pbx_system)
+    db.session.commit()
+
+
 def get_registered_pbx_numbers(enabled=True, departments: List[Department] = None):
-    pbx_numbers_stmt = PBXNumber.query.filter_by(enabled=enabled)
+    pbx_numbers_stmt = PBXNumber.query.filter()
+    if is_boolean(enabled):
+        is_enabled = convert_to_boolean(enabled)
+        pbx_numbers_stmt = pbx_numbers_stmt.filter_by(enabled=is_enabled)
+
     if departments:
         pbx_numbers_stmt = pbx_numbers_stmt.filter(or_(PBXNumber.department == department for department in departments))
 
@@ -49,7 +99,26 @@ def get_registered_pbx_numbers(enabled=True, departments: List[Department] = Non
 
 
 def get_registered_pbx_number_records(enabled=True):
-    return PBXNumber.query.filter_by(enabled=enabled).all()
+    pbx_numbers_stmt = PBXNumber.query.filter()
+    if is_boolean(enabled):
+        is_enabled = convert_to_boolean(enabled)
+        pbx_numbers_stmt = pbx_numbers_stmt.filter_by(enabled=is_enabled)
+
+    return pbx_numbers_stmt.all()
+
+
+def register_pbx_system(data):
+    new_pbx_system = PBXSystem(
+        public_id=str(uuid.uuid4()),
+        name=data.get('name'),
+        enabled=data.get('enabled'),
+        inserted_on=datetime.datetime.utcnow(),
+        updated_on=datetime.datetime.utcnow(),
+    )
+    db.session.add(new_pbx_system)
+    db.session.commit()
+
+    return new_pbx_system
 
 
 def register_pbx_number(data):
